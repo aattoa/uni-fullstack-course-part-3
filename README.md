fac8ada0297b9c4e20e9c54835fd8a113e20f795
aattoa <aattoa@proton.me>
2024-11-03 12:29:29 +0200

    cors

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fullstack kurssin osa 3 palautusrepositorio
+
+Online at [uni-fullstack-course-part-3.onrender.com](https://uni-fullstack-course-part-3.onrender.com/api/persons)
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const persons = [
   {
@@ -34,6 +35,7 @@ const add_person = (name, number) => {
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('requestbody', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :requestbody'))
